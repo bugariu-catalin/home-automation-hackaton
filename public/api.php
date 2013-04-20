7e642ed51d59a3a1f10c1e16d5b11c06cdefd7ad
bugariu-catalin <bugariu.catalin@deluxemedia.ro>
2013-04-21 02:54:23 +0300

    Fix small error

--- public/api.php
+++ public/api.php
@@ -4,6 +4,7 @@ if ($_REQUEST['get_status']==1) {
 	$a = array();
 	$f = file('/home/pi/.all_status');
 	foreach ($f as $item) {
+		$item = str_replace(array("\r","\n"),"",$item);
 		$item = explode('=',$item);
 		$a[$item[0]] = $item[1];
 	}
